15bc46ec585d423852c26ca694eaaaefa38fd5d8
Assaf Tamir <ATamir@synamedia.com>
2022-05-15 15:51:30 +0300

    h

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	for i := 0; i < 100; i++ {
 		game := Game{[boardSize][boardSize]*Token{}, players, 0, 0}
 		game.newGame()
-		isWinner, winner := game.game(false)
+		isWinner, winner := game.game(true)
 		if isWinner {
 			fmt.Println(winner.char, " win!, Number of moves: ", game.moveCounter)
 			counter[winner.char]++
